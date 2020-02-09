61df710782286fdcb6dc0de3854cf4ea6526ab7f
Sen Ananthasayanan <sen.sayanan@gmail.com>
2020-02-09 15:14:10 +0000

    Added lights control and alerts

--- huebell.py
+++ huebell.py
@@ -1,46 +1,83 @@
 from ring_doorbell import Ring, Auth
+from phue import Bridge
 import json
-import datetime
-import pytz
+from time import sleep
+import sys
 
-# Get log in credentials from config file
+# Get config variables from config file
 with open('config.json') as config_file:
     data = json.load(config_file)
 
-RING_USERNAME = data['RING_USER']
-RING_PASSWORD = data['RING_PASS']
+# Connect to Hue Bridge
+b = Bridge(data['HUE_IP'])
+
+# First time need to press the button on the Bridge and connect (within 30 seconds)
+# b.connect()
+
+lights = []
+hue_lights = b.lights
+
+print("[Hue] Connected")
 
 # Sign in to Ring
+RING_USERNAME = data['RING_USER']
+RING_PASSWORD = data['RING_PASS']
 auth = Auth("huebell/v1")
 auth.fetch_token(RING_USERNAME, RING_PASSWORD)
 ring = Ring(auth)
 ring.update_data()
+print("[Ring] Connected")
 
-# Load doorbell and recent events
+# Load doorbell
 devices = ring.devices()
 doorbell = devices['doorbots'][0]
 
-print("\nLatest events:\n")
-for event in doorbell.history(5):
-    print(event['id'])
-    print(event['kind'])
-    print(event['created_at'])
-    print('-'*30, "\n")
+
+def flash_lights():
+    # Get current status of all lights
+    for l in hue_lights:
+        light = {
+            'name': l.name,
+            'light': l,
+            'status': b.get_light(l.name, 'on'),
+            'bright': b.get_light(l.name, 'bri')
+        }
+        lights.append(light)
+
+    # Turn on hallway light
+    b.set_light(data['HUE_LIGHT'], {'on': True, 'bri': 250})
+
+    # Flash all lights
+    for light in lights:
+        if light['name'] != data['HUE_LIGHT']:
+            b.set_light(light['name'], 'on', True)
+            b.set_light(light['name'], 'bri', 250)
+    sleep(3)
+    for light in lights:
+        if light['name'] != data['HUE_LIGHT']:
+            b.set_light(light['name'], 'bri', light['bright'])
+            if light['status'] == False:
+                b.set_light(light['name'], 'on', False)
+
 
 # Check for doorbell ding events
-print("Checking the doorbell...\n")
+print("Checking for doorbell...\n")
+ding = False
 
-utc = pytz.UTC
-now = utc.localize(datetime.datetime.now())
-alert = False
-
-while not alert:
-    for event in doorbell.history(1, kind='ding'):
-        if event and event['created_at'] > now:
+try:
+    while not ding:
+        ring.update_dings()
+        alert = ring.active_alerts()
+        if alert and alert[0]['kind'] == 'ding':
             print("*"*30, "Someone is at the door!", "*"*30, "", sep="\n")
+            flash_lights()
+            event = doorbell.history(limit=1, kind='ding')[0]
             print(event)
             doorbell.recording_download(doorbell.history(limit=10, kind='ding')[0]['id'],
                                         filename='last_ding.mp4',
                                         override=True)
-            print("\nLatest alert video downloaded")
-            alert = True
+            print("\n[Ring] Latest alert video downloaded")
+            ding = True
+except KeyboardInterrupt:
+    print("\nClosing down")
+    sys.exit(0)
